0f23a454541915af5b448ed16a76f1a4321f93ba
monwire <monwire.dev@gmail.com>
2025-09-20 19:07:36 -0300

    Implementando as funcionalidades dos métodos PUT e PATCH

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -11,7 +11,9 @@ export function routeHandler(request, response) {
 
   if (route) {
     const routesParams = request.url.match(route.path)
-    const { query } = routesParams.groups
+    const { query, ...params } = routesParams.groups
+
+    request.params = params
     request.query = query ? extractQueryParams(query) : {}
     return route.controller({ request, response, database })
   }
